627540ae571ea651e7fed22f10c2bc538a682f03
Layomi <mac@MACs-MacBook-Pro.local>
2025-04-28 10:43:59 +0100

    popular blogs implemented

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+const rewriter = jsonServer.rewriter({
+  "/api/*": "/$1",
+  "/blogs/search/:term": "/blogs?q=:term", // This could override the default behavior
+});
+
 server.use(middlewares);
 server.use(cors()); // Enable CORS for all origins
 server.use(jsonServer.bodyParser);
-
+server.use(rewriter);
 server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running on http://localhost:3000");
