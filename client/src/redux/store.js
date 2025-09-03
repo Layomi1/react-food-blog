9af6f52e641eb1f026eb7061ad6dff23da29834e
Layomi <layomilawore@gmail.com>
2025-09-03 20:20:59 +0100

    removing an unnnecessary importnfrom store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import blogsReducer from "./features/blogs/blogsSlice";
 import blogReducer from "./features/singleBlog/blogSlice";
 import filterReducer from "./features/filter/filterSlice";
-import logger from "redux-logger";
 
 export const store = configureStore({
   reducer: {
