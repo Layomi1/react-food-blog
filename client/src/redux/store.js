343b64a4417b7326921f1a478d9fb52a77a379d5
Layomi <layomilawore@gmail.com>
2025-09-04 09:46:43 +0100

    adding db.json in server

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,12 +3,7 @@ import blogsReducer from "./features/blogs/blogsSlice";
 import blogReducer from "./features/singleBlog/blogSlice";
 import filterReducer from "./features/filter/filterSlice";
 
-let middleware = [];
-
-if (process.env.NODE_ENV === "development") {
-  const { logger } = require("redux-logger");
-  middleware.push(logger);
-}
+import { logger } from "redux-logger";
 
 export const store = configureStore({
   reducer: {
@@ -17,5 +12,7 @@ export const store = configureStore({
     filter: filterReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(middleware),
+    getDefaultMiddleware().concat(
+      import.meta.env.MODE === "development" ? logger : []
+    ),
 });
--- server/server.js
+++ server/server.js
@@ -1,10 +1,30 @@
 import jsonServer from "json-server";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = jsonServer.create();
-const router = jsonServer.router("server/db.json");
+
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 server.use(router);
 
 export default server;
